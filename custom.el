fc98c84716907436fd6c70c2afe99c136a9fff06
Pedro Schreiber <schreiber.mmb@gmail.com>
2021-08-05 18:36:58 -0300

    first commit - set emacs configuration files

---
+++ custom.el
@@ -0,0 +1,50 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#1e1e1e" "#D16969" "#579C4C" "#D7BA7D" "#339CDB" "#C586C0" "#85DDFF" "#d4d4d4"])
+ '(custom-safe-themes
+   '("0398e059d76b33863917ef39a4a2dcb71a12420d20dace57f63bb190c84436db" default))
+ '(fci-rule-color "#37474F")
+ '(jdee-db-active-breakpoint-face-colors (cons "#171F24" "#237AD3"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#171F24" "#579C4C"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#171F24" "#777778"))
+ '(objed-cursor-color "#D16969")
+ '(pdf-view-midnight-colors (cons "#d4d4d4" "#1e1e1e"))
+ '(recentf-save-file (concat doom-cache-dir "recentf") nil nil "Customized with use-package recentf")
+ '(rustic-ansi-faces
+   ["#1e1e1e" "#D16969" "#579C4C" "#D7BA7D" "#339CDB" "#C586C0" "#85DDFF" "#d4d4d4"])
+ '(save-place-file (concat doom-cache-dir "saveplace") nil nil "Customized with use-package saveplace")
+ '(savehist-file (concat doom-cache-dir "savehist") nil nil "Customized with use-package savehist")
+ '(server-auth-dir (concat doom-emacs-dir "server/") nil nil "Customized with use-package server")
+ '(undo-fu-session-directory (concat doom-cache-dir "undo-fu-session/") nil nil "Customized with use-package undo-fu-session")
+ '(vc-annotate-background "#1e1e1e")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#579C4C")
+    (cons 40 "#81a65c")
+    (cons 60 "#acb06c")
+    (cons 80 "#D7BA7D")
+    (cons 100 "#d8ab79")
+    (cons 120 "#d99c76")
+    (cons 140 "#DB8E73")
+    (cons 160 "#d38b8c")
+    (cons 180 "#cc88a6")
+    (cons 200 "#C586C0")
+    (cons 220 "#c97ca3")
+    (cons 240 "#cd7286")
+    (cons 260 "#D16969")
+    (cons 280 "#ba6c6c")
+    (cons 300 "#a37070")
+    (cons 320 "#8d7374")
+    (cons 340 "#37474F")
+    (cons 360 "#37474F")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
